8533ef3cfffeb0a96e14fc529d1495aada26592c
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-09-20 23:49:53 +0300

    початок 10ї заново

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputRefs: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+refs.inputRefs.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
