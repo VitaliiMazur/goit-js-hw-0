b6edfd443fecb30259e39dd326f08059922d852e
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-09-21 12:20:12 +0300

    в процесі

--- src/index.js
+++ src/index.js
@@ -6,8 +6,20 @@ import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
-  inputRefs: document.querySelector('#search-box'),
+  inputField: document.querySelector('#search-box'),
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
-refs.inputRefs.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+refs.inputField.addEventListener(
+  'input',
+  debounce(inputElement, DEBOUNCE_DELAY)
+);
+
+function inputElement(evt) {
+  const inputValue = evt.target.value;
+
+  fetchCountries(inputValue).then(data => {
+    console.log(data);
+  });
+}
